a89572a7ae2fdb2e36a63fe6ab4d17d1853379d0
jianan <jianan168@berkeley.edu>
2023-10-28 03:03:04 -0700

    new files

---
+++ app.py
@@ -0,0 +1,14 @@
+
+
+
+def main():
+    while True:
+        query = input('> ')
+        if query == 'exit':
+            break
+
+        else:
+            print(query)
+
+if __name__ == '__main__':
+    main()
---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+os.environ['OPENAI_API_KEY'] = 'API key here'
+
+os.environ['MINDSDB_API_KEY'] = 'API key here'
+
+os.environ['NUMPY_API_KEY'] = 'API key here'
+
+os.environ['FAST_API_KEY'] = 'API key here'
+
+os.environ['GOOGLE_API_KEY'] = 'API key here'
+
+os.environ['WIKIPEDIA_API_KEY'] = 'API key here'
